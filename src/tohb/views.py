cebca00e1ce076ea221aaff590eda6cc7ba58235
OtbeyNat <tant3@rpi.edu>
2024-11-07 12:27:43 -0500

    change order

--- src/tohb/views.py
+++ src/tohb/views.py
@@ -10,13 +10,13 @@ def home_page_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     pageqs = PageVisit.objects.filter(path=request.path)
     my_title = "My Page"
+    html_template = "home.html"
     my_context = {
         "title": my_title,
         "page_visit_count": pageqs.count(),
         "percent": (pageqs.count() * 100.0) / qs.count(),
         "total_visit_count": qs.count(),
     }
-    html_template = "home.html"
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
 

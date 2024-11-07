f428b0a5ad563ac55e599dd7fc0937775c189f72
OtbeyNat <tant3@rpi.edu>
2024-11-07 12:41:33 -0500

    prepare for railway

--- src/tohb/views.py
+++ src/tohb/views.py
@@ -6,24 +6,23 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     pageqs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (pageqs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
     my_title = "My Page"
     html_template = "home.html"
     my_context = {
         "title": my_title,
         "page_visit_count": pageqs.count(),
-        "percent": (pageqs.count() * 100.0) / qs.count(),
+        "percent": percent,
         "total_visit_count": qs.count(),
     }
     PageVisit.objects.create(path=request.path)
-    return render(request, html_template, my_context)
-
-def about_page_view(request, *args, **kwargs):
-    my_title = "My Page"
-    my_context = {
-        "title": my_title,
-    }
-    html_template = "about.html"
     return render(request, html_template, my_context)
